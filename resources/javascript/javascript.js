b8a8476848ceafeafeceb2b6de52245967487761
Fisher <jackfisher94@live.co.uk>
2019-01-16 18:58:15 +0000

    Added my imagery to slick

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
+
+// slick slideshow for gallery page
+
 $('.image-container-slick').slick({
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 800,
     fade: true,
     cssEase: 'linear',
     accessibility: false,
-    autoplaySpeed: 4000,
+    autoplay: true,
+    autoplaySpeed: 7000,
     draggable: false,
     pauseOnFocus: false,
     pauseOnHover: false,
     swipe: false,
     touchMove: false,
   });
+
+
+// lazy loading of images 
+
+window.addEventListener("load", function(event) {
+    lazyload();
+});
+
+
+
 
 });
