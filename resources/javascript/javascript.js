5e0ae4b5a1116620db757f000d776b68e1bef772
Fisher <jackfisher94@live.co.uk>
2019-01-16 15:54:08 +0000

    Added slick plugin to gallery

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+$('.image-container-slick').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    accessibility: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    swipe: false,
+    touchMove: false,
+  });
 
+});
