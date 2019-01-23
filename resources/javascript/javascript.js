1421915c5d08f9fa4aabc4c16691d2e299ca0faa
Fisher <jackfisher94@live.co.uk>
2019-01-23 18:04:12 +0000

    added javascript menu to homepage

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -1,23 +1,33 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-// slick slideshow for gallery page
+// // slick slideshow for gallery page
 
-$('.image-container-slick').slick({
-    dots: true,
-    infinite: true,
-    speed: 800,
-    fade: true,
-    cssEase: 'linear',
-    accessibility: false,
-    autoplay: true,
-    autoplaySpeed: 7000,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    swipe: true,
-    arrows: false,
-    touchMove: false,
-  });
+// $('.image-container-slick').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 800,
+//     fade: true,
+//     cssEase: 'linear',
+//     accessibility: false,
+//     autoplay: true,
+//     autoplaySpeed: 7000,
+//     draggable: false,
+//     pauseOnFocus: false,
+//     pauseOnHover: false,
+//     swipe: true,
+//     arrows: false,
+//     touchMove: false,
+//   });
 
 
-});
+// });
+
+var menu = document.querySelector('#menu');
+var menuItems = document.querySelector('ul');
+var text = document.querySelector('.hero-text');
+
+
+menu.addEventListener('click', function() {
+  menuItems.classList.add('open');
+  text.classList.add('open');
+})
