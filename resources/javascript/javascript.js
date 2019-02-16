5ab445fe9f1a88682e41fd58be35615993a250b3
Fisher <jackfisher94@live.co.uk>
2019-02-16 09:54:29 +0000

    animation adjustment

--- resources/javascript/javascript.js
+++ resources/javascript/javascript.js
@@ -15,69 +15,54 @@ $(document).ready(function() {
     pauseOnHover: false,
     swipe: true,
     arrows: false,
-    touchMove: false
+    touchMove: false,
   });
 });
- // Animate on scroll  /////////////////
+// Animate on scroll  /////////////////
 
-
-
-var $animation_elements = $('.animation-element');
+var $animation_elements = $(".animation-element");
 var $window = $(window);
 
-$window.on('scroll', check_if_in_view);
+$window.on("scroll", check_if_in_view);
 
-$window.trigger('scroll');
+$window.trigger("scroll");
 
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = window_top_position + window_height;
 
-  function check_if_in_view() {
-    var window_height = $window.height();
-    var window_top_position = $window.scrollTop();
-    var window_bottom_position = (window_top_position + window_height);
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = element_top_position + element_height;
 
-    $.each($animation_elements, function () {
-      var $element = $(this);
-      var element_height = $element.outerHeight();
-      var element_top_position = $element.offset().top;
-      var element_bottom_position = (element_top_position + element_height);
+    if (
+      element_bottom_position >= window_top_position &&
+      element_top_position <= window_bottom_position
+    ) {
+      $element.addClass("in-view");
+    } else {
+      $element.removeClass("in-view");
+    }
+  });
+}
 
-      if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-        $element.addClass('in-view');
-      } else {
-        $element.removeClass('in-view');
-      }
-    });
-  } 
+// open and close menu items when menu is clicked 
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  var menu = document.querySelector("#menu");
+  var menuItems = document.querySelector(".menu-items");
+  var text = document.querySelector(".hero-text");
 
-
-
-
-document.addEventListener("DOMContentLoaded", function (event) {
-  
-  var menu = document.querySelector('#menu');
-  var menuItems = document.querySelector('.menu-items');
-  var text = document.querySelector('.hero-text');
-
-
-  menu.addEventListener('click', function () {
-    if (menuItems.classList.contains('open')) {
-      menuItems.classList.remove('open');
+  menu.addEventListener("click", function() {
+    if (menuItems.classList.contains("open")) {
+      menuItems.classList.remove("open");
       text.classList.remove("open");
     } else {
       menuItems.classList.add("open");
-      text.classList.add('open')
-    };
-
-
+      text.classList.add("open");
+    }
   });
-
 });
-
-
-
-
-
-
-
